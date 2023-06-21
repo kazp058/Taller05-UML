11e4c942cf07a9b3f054ddd60ccf915ebb7752ec
kazp058 <kazp058@gmail.com>
2023-06-21 16:11:48 -0500

    Clase estudiante

---
+++ Taller05UML/src/main/java/com/espol/clases/Estudiante.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.clases;
+
+import com.espol.abstractas.ActividadSumativa;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Estudiante {
+    public void solicitarInscripcionCurso(Curso curso){
+        //Codigo
+    }
+    
+    public void realizarActividadSumativa(Curso curso, ActividadSumativa actividadSumativa){
+        //Codigo
+    }
+}
