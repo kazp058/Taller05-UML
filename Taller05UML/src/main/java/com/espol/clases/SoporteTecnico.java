f9b75e102f4f573fab1c3bc95694c86b4fd04a73
kazp058 <kazp058@gmail.com>
2023-06-21 16:19:01 -0500

    Clase soporte tecnico

---
+++ Taller05UML/src/main/java/com/espol/clases/SoporteTecnico.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.clases;
+
+/**
+ *
+ * @author CltControl
+ */
+public class SoporteTecnico {
+    public void brindarAsistencia(Usuario usuario){
+        //Codigo
+    }
+    
+    public void resolverProblemaTecnico(Usuario usuario){
+        //Codigo
+    }
+    
+    public void derivarProblemaDeGestion(Administrador administrador){
+        //Codigo
+    }
+    
+}
