713d25024d54b1d1dec410c1b312d547d6f78b27
kazp058 <kazp058@gmail.com>
2023-06-21 16:11:57 -0500

    Clase evaluacion

---
+++ Taller05UML/src/main/java/com/espol/clases/Evaluacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.clases;
+
+import com.espol.abstractas.ActividadSumativa;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Evaluacion extends ActividadSumativa {
+    private int limiteDeTiempo;
+}
