8a60fce8fb0e06477c64ab7d25902aecf6ae3922
kazp058 <kazp058@gmail.com>
2023-06-21 16:12:33 -0500

    Clase profesor

---
+++ Taller05UML/src/main/java/com/espol/clases/Profesor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.clases;
+
+import com.espol.abstractas.ActividadSumativa;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Profesor {
+    
+    private Curso[] cursosACargo;
+    
+    public void cargarActividadSumativa(Curso curso, ActividadSumativa actividadSumativa){
+        //Codigo
+    }
+    
+    public void calificarActividadSumativa(Curso curso, Estudiante estudiante, ActividadSumativa actividadSumativa) {
+        //Codigo
+    }
+    
+    public void aceptarInscripcion(Curso curso, String usuarioEstudiante){
+        //Codigo
+    }
+    
+    public void crearNuevoForo(Foro foro){
+        //Codigo
+    }
+}
