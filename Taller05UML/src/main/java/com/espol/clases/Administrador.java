52b089546a263ff02d35ab449d19a2328b1f56da
kazp058 <kazp058@gmail.com>
2023-06-21 16:18:53 -0500

    Clase administrador

---
+++ Taller05UML/src/main/java/com/espol/clases/Administrador.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.clases;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Administrador {
+    public void crearUsuarioConRol(String usuario, String contrasena){
+        //Codigo
+    }
+    
+    public void asignarPermiso(String usuario){
+        //Codigo
+    }
+    
+    public void actualizarSoftware(){
+        //Codigo
+    }
+    
+    public void asignarResponsable(Curso curso, Profesor responsable){
+        //Codigo
+    }
+}
