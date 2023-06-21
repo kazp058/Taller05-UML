38aa1ce5eadae9b665365d0737204db3b958eb5e
kazp058 <kazp058@gmail.com>
2023-06-21 16:11:33 -0500

    Actividad sumativa clase

---
+++ Taller05UML/src/main/java/com/espol/abstractas/ActividadSumativa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.abstractas;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author CltControl
+ */
+public abstract class ActividadSumativa {
+    protected String titulo;
+    protected LocalDateTime fechaDeEntrega;
+    protected float puntajeMaximo;
+    protected String contenido;
+    protected float calificacion;
+    protected String id;
+}
