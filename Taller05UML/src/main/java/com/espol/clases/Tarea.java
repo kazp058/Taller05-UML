29365e2e0e6c80271924a343cb3b44b208bb4f5b
kazp058 <kazp058@gmail.com>
2023-06-21 16:12:40 -0500

    Clase tarea

---
+++ Taller05UML/src/main/java/com/espol/clases/Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.clases;
+
+import com.espol.abstractas.ActividadSumativa;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Tarea extends ActividadSumativa{
+    private LocalDateTime fechaDePublicacion;
+}
