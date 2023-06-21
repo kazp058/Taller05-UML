32a28112e1a4f7b44bf568ad2d9e2ef16ba6f43d
kazp058 <kazp058@gmail.com>
2023-06-21 16:12:25 -0500

    Clase personal academico

---
+++ Taller05UML/src/main/java/com/espol/clases/PersonalAcademico.java
@@ -0,0 +1,22 @@
+package com.espol.clases;
+
+/**
+ *
+ * @author CltControl
+ */
+public class PersonalAcademico extends Usuario{
+    
+    protected Incidente[] incidentes;
+    
+    public void reportarError(String email, String contenido){
+        //Codigo
+    }
+    
+    public void solicitarAsesoramientoDelSistema(String email, String contenido){
+        //Codigo
+    }
+    
+    public void comentar(Foro foro, String comentario){
+        //Codigo
+    }
+}
